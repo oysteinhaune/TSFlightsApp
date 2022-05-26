a4cb46e6f65acfb535e17a4b17104ce5a3045fa8
Øystein <oysteinha@gmail.com>
2022-05-26 22:20:29 +0200

    Add home, admin component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,22 @@
+import { Routes } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AdminComponent } from './admin/admin.component';
+
+const appRoutes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'admin', component: AdminComponent },
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AdminComponent
   ],
   imports: [
     BrowserModule,
