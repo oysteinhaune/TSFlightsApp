6f4346f0de45f6cb4df387b746215bef4b35359c
Øystein <oysteinha@gmail.com>
2022-05-28 10:46:04 +0200

    Add flight service

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,6 @@
+import { FlightsService } from './../flights.service';
 import { Component, OnInit } from '@angular/core';
+import { FlightsService } from '../flights.service';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +9,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private flightsService: FlightsService) { }
 
   ngOnInit(): void {
+  }
+
+  getFlights() {
+
   }
 
 }
